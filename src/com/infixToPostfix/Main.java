0a5e7f1177a4d33817189a6daa6c116e4bbf8cb7
fahritech04 <fahricoding@gmail.com>
2023-03-19 16:53:49 +0700

    commit

---
+++ src/com/infixToPostfix/Main.java
@@ -0,0 +1,83 @@
+﻿package com.infixToPostfix;
+
+import java.util.*;
+
+public class Main {
+
+  // Fungsi untuk menentukan prioritas operator
+  public static int priority(char op) {
+    if (op == '+' || op == '-')
+      return 1;
+    else if (op == '*' || op == '/')
+      return 2;
+    else if (op == '^')
+      return 3;
+    else
+      return 0;
+  }
+
+  // Fungsi untuk mengubah notasi infix menjadi postfix
+  public static String infixToPostfix(String infix) {
+    // Inisialisasi stack untuk operator
+    Stack<Character> operatorStack = new Stack<>();
+
+    // Inisialisasi deque untuk output postfix
+    Deque<Character> postfixDeque = new ArrayDeque<>();
+
+    // Loop untuk memproses setiap karakter dalam notasi infix
+    for (int i = 0; i < infix.length(); i++) {
+      char c = infix.charAt(i);
+
+      // Jika karakter adalah operand, tambahkan ke deque postfix
+      if (Character.isLetterOrDigit(c)) {
+        postfixDeque.add(c);
+      }
+
+      // Jika karakter adalah kurung buka, tambahkan ke stack operator
+      else if (c == '(') {
+        operatorStack.push(c);
+      }
+
+      // Jika karakter adalah kurung tutup, pop stack operator sampai menemukan kurung
+      // buka
+      else if (c == ')') {
+        while (!operatorStack.isEmpty() && operatorStack.peek() != '(') {
+          postfixDeque.add(operatorStack.pop());
+        }
+        if (!operatorStack.isEmpty() && operatorStack.peek() != '(') {
+          return "Not valid";
+        } else {
+          operatorStack.pop();
+        }
+      }
+
+      // Jika karakter adalah operator, pop stack operator sampai menemukan operator
+      // dengan prioritas yang lebih rendah atau stack kosong
+      else {
+        while (!operatorStack.isEmpty() && priority(c) <= priority(operatorStack.peek())) {
+          postfixDeque.add(operatorStack.pop());
+        }
+        operatorStack.push(c);
+      }
+    }
+
+    // Pop sisa operator di stack dan tambahkan ke deque postfix
+    while (!operatorStack.isEmpty()) {
+      postfixDeque.add(operatorStack.pop());
+    }
+
+    // Konversi deque postfix menjadi string
+    StringBuilder postfix = new StringBuilder();
+    for (char c : postfixDeque) {
+      postfix.append(c);
+    }
+
+    return postfix.toString();
+  }
+
+  public static void main(String[] args) {
+    String infix = "a+b*(c^d-e)^(f+g*h)-i";
+    String postfix = infixToPostfix(infix);
+    System.out.println(postfix);
+  }
+}
---
+++ src/com/toDoList/Main.java
@@ -0,0 +1,69 @@
+﻿package com.toDoList;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+ public static void main(String[] args) {
+  ArrayList<String> tasks = new ArrayList<String>();
+  Scanner scanner = new Scanner(System.in);
+  int choice;
+
+  do {
+   System.out.println("Pilih menu: ");
+   System.out.println("1. Tambah tugas");
+   System.out.println("2. Tampilkan tugas yang harus diselesaikan");
+   System.out.println("3. Hapus tugas yang sudah diselesaikan");
+   System.out.println("4. Tampilkan seluruh isi to-do list");
+   System.out.println("5. Keluar");
+
+   choice = scanner.nextInt();
+
+   switch (choice) {
+    case 1:
+     System.out.print("Masukkan nama tugas: ");
+     scanner.nextLine();
+     String task = scanner.nextLine();
+     tasks.add(task);
+     System.out.println("Tugas \"" + task + "\" telah ditambahkan ke dalam to-do list.");
+     break;
+    case 2:
+     if (tasks.isEmpty()) {
+      System.out.println("To-do list kosong.");
+     } else {
+      System.out.println("Tugas yang harus diselesaikan: " + tasks.get(0));
+     }
+     break;
+    case 3:
+     if (tasks.isEmpty()) {
+      System.out.println("To-do list kosong.");
+     } else {
+      String taskCompleted = tasks.remove(0);
+      System.out.println("Tugas \"" + taskCompleted + "\" telah dihapus dari to-do list.");
+     }
+     break;
+    case 4:
+     if (tasks.isEmpty()) {
+      System.out.println("To-do list kosong.");
+     } else {
+      System.out.println("Isi to-do list:");
+      for (String taskItem : tasks) {
+       System.out.println("- " + taskItem);
+      }
+     }
+     break;
+    case 5:
+     System.out.println("Keluar dari program.");
+     break;
+    default:
+     System.out.println("Pilihan tidak valid. Silakan pilih lagi.");
+     break;
+   }
+
+   System.out.println();
+
+  } while (choice != 5);
+
+  scanner.close();
+ }
+}
